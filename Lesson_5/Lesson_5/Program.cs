143b795d8565edaba3576e7446bc5ea83916a3f7
FW960 <artuninp@gmail.com>
2022-02-11 03:22:44 +0800

    Создал Шаблон Консольной Команды

---
+++ Lesson_5/Lesson_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Lesson_5
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
