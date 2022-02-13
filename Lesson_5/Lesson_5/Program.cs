fe86793038dda2d74d7a9a258e0da27a65d187cd
FW960 <artuninp@gmail.com>
2022-02-14 04:55:33 +0800

    Сериализация В Бинарном Файле

--- Lesson_5/Lesson_5/Program.cs
+++ Lesson_5/Lesson_5/Program.cs
@@ -2,15 +2,51 @@
 using Newtonsoft.Json;
 using System.IO;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+
 
 namespace Lesson_5
 
 {
+    [Serializable]
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Введите число от 0 до 255: ");
+
+            int UserNum = 0;
+
+            try
+            {
+                UserNum = Convert.ToInt32(Console.ReadLine());
+
+
+
+                if (UserNum > 255)
+                {
+                    Console.WriteLine("Введите число в заданном диапазоне");
+
+                    return;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Введите корректное число.");
+
+                return;
+
+            }
+
+            string fileWay = @"C:\Users\windo\source\repos\Lesson_5\Lesson_5\Lesson_5\NewFile.bin";
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(new FileStream(fileWay, FileMode.OpenOrCreate), UserNum);
+
+            Console.WriteLine("Число успешно сериализовано в файл");
+
         }
     }
 }
